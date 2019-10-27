faa676eb67511162c1a4bc254b50f12d82d0854b
antuan159 <lanos159@ukr.net>
2019-10-27 16:35:15 +0200

    Updates

---
+++ precache-manifest.3f857271f60bb36ab6d2b7ad2fc6a487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b6efae964c834571c5c7699550edd5",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "87513279197abc6f6103",
+    "url": "/goit-react-hw-02-reader/static/css/main.b486304e.chunk.css"
+  },
+  {
+    "revision": "3f9aba0de64952e56eee",
+    "url": "/goit-react-hw-02-reader/static/js/2.f86f5e60.chunk.js"
+  },
+  {
+    "revision": "87513279197abc6f6103",
+    "url": "/goit-react-hw-02-reader/static/js/main.a0028f6b.chunk.js"
+  },
+  {
+    "revision": "0964f7f4a164b8ecf671",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.8e8ab307.js"
+  }
+]);
